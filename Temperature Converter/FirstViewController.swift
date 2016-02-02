3b60343f484ce9e4fb40589da3c4a8686e97f222
Clayton Harper <Clayton_Harper95@yahoo.com>
2016-02-02 12:58:41 -0700

    finish basic project

--- Temperature Converter/FirstViewController.swift
+++ Temperature Converter/FirstViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var cOutputTextView: UITextView!
+    @IBOutlet weak var cInputTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        cOutputTextView.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,27 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Function to convert Celsius to Fehrenheit
+    func celcsiusToFehrenheit(tempCelsius: Double) -> Double {
+            return tempCelsius * 1.8 + 32
+    }
+    
+    @IBAction func cConvertButtonPressed(sender: AnyObject) {
+        // If valid input, update the label
+        // else invalid input, run error message
+        if let userInput: Double = Double(cInputTextField.text!) {
+             cOutputTextView.text = "\(userInput) in Celsius is \(celcsiusToFehrenheit(userInput)) in Fehrenheit"
+        } else {
+            cOutputTextView.text = "Invalid input"
+        }
+    }
+    
+    @IBAction func cResetButtonPressed(sender: AnyObject) {
+        // Clear all input from inputTextField
+        // Reset label to initial state
+        cInputTextField.text = ""
+        cOutputTextView.text = ""
+    }
+    
 }
 
--- Temperature Converter/SecondViewController.swift
+++ Temperature Converter/SecondViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var fOutputTextView: UITextView!
+    @IBOutlet weak var fInputTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fOutputTextView.text = ""
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func fahrenheitToCelsius(tempFahrenheit: Double) -> Double {
+        return (tempFahrenheit - 32) / 1.8
+    }
+    
+    @IBAction func fConvertButtonPressed(sender: AnyObject) {
+        if let userInput: Double = Double(fInputTextField.text!) {
+            fOutputTextView.text = "\(userInput) in Celsius is \(fahrenheitToCelsius(userInput)) in Fehrenheit"
+        } else {
+            fOutputTextView.text = "Invalid input"
+        }
+    }
+    
+    @IBAction func fResetButtonPressed(sender: AnyObject) {
+        fInputTextField.text = ""
+        fOutputTextView.text = ""
+    }
 }
 
