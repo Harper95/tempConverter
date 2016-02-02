cd8b3a163ac6dfbae413ee660dc3bdd96674c77f
Clayton Harper <Clayton_Harper95@yahoo.com>
2016-02-02 11:25:25 -0700

    Initial Commit

---
+++ Temperature Converter/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Temperature Converter
+//
+//  Created by Clayton Harper on 2/2/16.
+//  Copyright © 2016 Clayton Harper. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Temperature Converter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Temperature Converter
+//
+//  Created by Clayton Harper on 2/2/16.
+//  Copyright © 2016 Clayton Harper. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
